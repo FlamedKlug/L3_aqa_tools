7d9a5e00a1238eb8affea4603519f880332efca5
Anton Maloivan <kluge@bk.ru>
2025-08-03 17:24:38 +0300

    Тесты запускаются с разными сессиями браузера

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture()
 def browser_setup():
     browser.config.window_width = 800
     browser.config.window_height = 600
